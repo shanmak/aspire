09f1db1553264c94f972aed990bff94428245c04
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-12-20 07:50:35 +0800

    Updates

---
+++ precache-manifest.f9f1f01ff95cecc417428744352d0886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21d0f54f1700a1b796cd",
+    "url": "/aspire/static/js/runtime~main.21d0f54f.js"
+  },
+  {
+    "revision": "0ecfcba25e081d0fab83",
+    "url": "/aspire/static/js/main.0ecfcba2.chunk.js"
+  },
+  {
+    "revision": "0c5a549fffd4b76777ba",
+    "url": "/aspire/static/js/1.0c5a549f.chunk.js"
+  },
+  {
+    "revision": "0ecfcba25e081d0fab83",
+    "url": "/aspire/static/css/main.34be2080.chunk.css"
+  },
+  {
+    "revision": "0c5a549fffd4b76777ba",
+    "url": "/aspire/static/css/1.0245c0e4.chunk.css"
+  },
+  {
+    "revision": "417e153952d7a5242c0b48e738f2c52e",
+    "url": "/aspire/index.html"
+  }
+];
